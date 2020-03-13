f228a65ef9101302567629b48f3a985e757566c2
Nikolay <nickkrut1@gmail.com>
2020-03-13 14:16:09 +0300

    Just testing git

--- app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // creating main screen here
+        // testing git
+        int a = 10;
         setContentView(R.layout.activity_main);
     }
 }
