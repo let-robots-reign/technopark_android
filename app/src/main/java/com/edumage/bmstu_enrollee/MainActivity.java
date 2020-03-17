d1cff79d3349899056bdbd6619c1a90a4e997ff7
alexey zotov <zotov.alexei.1@yandex.ru>
2020-03-17 16:13:35 +0300

    1. BottomNavigationView 2. 3 Fragments

--- app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
@@ -1,7 +1,13 @@
 package com.edumage.bmstu_enrollee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_nav);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+        // keep selected fragment after rotation
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new HomeFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home_tab:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.catalog_tab:
+                            selectedFragment = new CatalogFragment();
+                            break;
+                        case R.id.stats_tab:
+                            selectedFragment = new StatsFragment();
+                            break;
+                    }
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, selectedFragment).commit();
+                    }
+                    return true;
+        }
+    };
 }
