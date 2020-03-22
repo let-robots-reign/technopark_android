0798b639cad73c92448c12acbfc34239aeb9cf01
Dima <p134d@mail.ru>
2020-03-22 18:21:55 +0300

    first_commit

---
+++ app/src/main/java/com/edumage/bmstu_enrollee/Fragments/LAFragmentFirst.java
@@ -0,0 +1,9 @@
+package com.edumage.bmstu_enrollee.Fragments;
+
+import androidx.fragment.app.Fragment;
+//LA means LaunchActivity
+public class LAFragmentFirst extends Fragment {
+
+
+
+}
---
+++ app/src/main/java/com/edumage/bmstu_enrollee/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.edumage.bmstu_enrollee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_launch);
+    }
+}
