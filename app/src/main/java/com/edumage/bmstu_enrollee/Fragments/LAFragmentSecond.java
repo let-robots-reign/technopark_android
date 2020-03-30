2ef19d5abeb4129693e68cd1c2ce07d37956a1b3
Dima <p134d@mail.ru>
2020-03-30 13:46:29 +0400

    final commit

---
+++ app/src/main/java/com/edumage/bmstu_enrollee/Fragments/LAFragmentSecond.java
@@ -0,0 +1,129 @@
+package com.edumage.bmstu_enrollee.Fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.edumage.bmstu_enrollee.EGEAdapter;
+import com.edumage.bmstu_enrollee.EGESubject;
+import com.edumage.bmstu_enrollee.R;
+import com.edumage.bmstu_enrollee.WelcomeActivity;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LAFragmentSecond extends Fragment implements WelcomeActivity.CompletableFragment {
+
+    private RecyclerView recyclerView;
+    private EGEAdapter adapter;
+    private ArrayList<EGESubject> data;
+
+
+
+    public static final String TAG="LAFragmentSecond";
+    private static final String DATA="SUBJECTS";
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+        if (savedInstanceState!=null){
+            Toast.makeText(getContext(), "savedInstanceState!=null",Toast.LENGTH_SHORT ).show();
+            ObjectInputStream objectInputStream = null;
+            try {
+                objectInputStream = new ObjectInputStream(
+                        new ByteArrayInputStream(savedInstanceState.getByteArray(DATA)));
+            data = (ArrayList<EGESubject>)objectInputStream.readObject();
+            objectInputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "Unable to deserialize",Toast.LENGTH_SHORT ).show();
+            }
+        } else {
+            if (data==null){
+            data = new ArrayList<>();
+            getEGEList();}
+        }
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.la_fragment2,container,false);
+
+    recyclerView = v.findViewById(R.id.ege_list);
+
+    adapter = new EGEAdapter(data);
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext(),RecyclerView.VERTICAL,false));
+
+    return v;
+    }
+
+    @Override
+    public void onResume() {
+        adapter.notifyDataSetChanged();
+        super.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        try {
+            ByteArrayOutputStream baos=new ByteArrayOutputStream();
+            ObjectOutputStream ObjOut =  new ObjectOutputStream(baos);
+            ObjOut.writeObject(adapter.getData());
+            ObjOut.flush();
+            outState.putByteArray(DATA,baos.toByteArray());
+        } catch (IOException e) {
+            Toast.makeText(getContext(), "Unable to serialize",Toast.LENGTH_SHORT ).show();
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    public static LAFragmentSecond newFragment(){
+        return new LAFragmentSecond();
+    }
+
+
+
+
+    // получение списка предметов ЕГЭ пользователя - нужно заменить
+     private void getEGEList(){
+
+        data.add(new EGESubject("Русский язык",R.drawable.informatics));
+        data.add(new EGESubject("Информатика",R.drawable.informatics));
+        data.add(new EGESubject("Математика",R.drawable.informatics));
+        data.add(new EGESubject("Физика",R.drawable.physics));
+         data.add(new EGESubject("Химия",R.drawable.chemistry));
+         data.add(new EGESubject("Математика",R.drawable.informatics));
+         data.add(new EGESubject("Физика",R.drawable.physics));
+         data.add(new EGESubject("Химия",R.drawable.chemistry));
+
+    }
+
+
+
+    @Override
+    public boolean isComplete() {
+        //условие заверешние
+        //выгрузка информации
+        return true;
+    }
+}
