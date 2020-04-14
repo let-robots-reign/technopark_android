ebf62504fd8ee88f6d92cde17cb825f739c16c25
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-14 11:10:53 +0400

    Feature: add network state check

--- app/src/main/java/com/edumage/bmstu_enrollee/Fragments/HomeFragment.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/Fragments/HomeFragment.java
@@ -1,13 +1,7 @@
 package com.edumage.bmstu_enrollee.Fragments;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -154,50 +148,4 @@ public class HomeFragment extends Fragment {
         }
         return position;
     }
-
-    private boolean isNetworkConnected() {
-        if (getActivity() == null) {
-            return false;
-        }
-        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT < 23) {
-                NetworkInfo ni = cm.getActiveNetworkInfo();
-                if (ni != null) {
-                    return (ni.isConnected() && (ni.getType() == ConnectivityManager.TYPE_WIFI || ni.getType() == ConnectivityManager.TYPE_MOBILE));
-                }
-            } else {
-                Network net = cm.getActiveNetwork();
-                if (net != null) {
-                    NetworkCapabilities nc = cm.getNetworkCapabilities(net);
-                    return (nc != null && (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
-                            || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)));
-                }
-            }
-        }
-        return false;
-    }
 }
-
-/*
-private boolean hasInternetAccess() {
-    if (isNetworkConnected()) {
-        try {
-            HttpURLConnection urlc = (HttpURLConnection)
-                    (new URL("http://clients3.google.com/generate_204")
-                            .openConnection());
-            urlc.setRequestProperty("User-Agent", "Android");
-            urlc.setRequestProperty("Connection", "close");
-            urlc.setConnectTimeout(1500);
-            urlc.connect();
-            return (urlc.getResponseCode() == 204 &&
-                    urlc.getContentLength() == 0);
-        } catch (IOException e) {
-            Log.e("Connection Check", "Error checking internet connection", e);
-        }
-    } else {
-        Log.d("Connection Check", "No network available!");
-    }
-    return false;
-}
- */
--- app/src/main/java/com/edumage/bmstu_enrollee/ViewModels/HomeFragmentViewModel.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/ViewModels/HomeFragmentViewModel.java
@@ -1,24 +1,39 @@
 package com.edumage.bmstu_enrollee.ViewModels;
 
+import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.edumage.bmstu_enrollee.ParsingRepo.CurrentFilesParsing;
 import com.edumage.bmstu_enrollee.ParsingRepo.CurrentScoresParsing;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragmentViewModel extends ViewModel {
+public class HomeFragmentViewModel extends AndroidViewModel {
     private final MutableLiveData<List<String>> scoresLiveData = new MutableLiveData<>();
     private final MutableLiveData<List<String>> filesLiveData = new MutableLiveData<>();
     private List<String> programsNames;
     private final Handler handler = new Handler(Looper.getMainLooper());
+
+    public HomeFragmentViewModel(@NonNull Application application) {
+        super(application);
+    }
 
     public void init(List<String> names) {
         programsNames = names;
@@ -41,10 +56,16 @@ public class HomeFragmentViewModel extends ViewModel {
                 final List<String> scores = new ArrayList<>();
                 String score = "Ошибка";
                 for (String name : programsNames) {
-                    try {
-                        score = CurrentScoresParsing.getInstance().parseScore(name);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    if (!isNetworkConnected()) {
+                        score = "Нет сети";
+                    } else if (!hasInternetAccess()) {
+                        score = "Нет интернета";
+                    } else {
+                        try {
+                            score = CurrentScoresParsing.getInstance().parseScore(name);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                     scores.add(score);
 
@@ -85,5 +106,43 @@ public class HomeFragmentViewModel extends ViewModel {
             }
         });
         thread.start();
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            if (Build.VERSION.SDK_INT < 23) {
+                NetworkInfo ni = cm.getActiveNetworkInfo();
+                if (ni != null) {
+                    return (ni.isConnected() && (ni.getType() == ConnectivityManager.TYPE_WIFI
+                            || ni.getType() == ConnectivityManager.TYPE_MOBILE));
+                }
+            } else {
+                Network net = cm.getActiveNetwork();
+                if (net != null) {
+                    NetworkCapabilities nc = cm.getNetworkCapabilities(net);
+                    return (nc != null && (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                            || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)));
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean hasInternetAccess() {
+        try {
+            HttpURLConnection urlc = (HttpURLConnection)
+                    (new URL("http://clients3.google.com/generate_204")
+                            .openConnection());
+            urlc.setRequestProperty("User-Agent", "Android");
+            urlc.setRequestProperty("Connection", "close");
+            urlc.setConnectTimeout(1500);
+            urlc.connect();
+            return (urlc.getResponseCode() == 204 &&
+                    urlc.getContentLength() == 0);
+        } catch (IOException e) {
+            Log.e("Connection Check", "Error checking internet connection", e);
+        }
+        return false;
     }
 }
