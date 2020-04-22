35c61bdf97259a1814c71aadfb06ac9d40d6dbf7
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-22 14:52:11 +0400

    Exit on second Back press

--- app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/MainActivity.java
@@ -3,16 +3,21 @@ package com.edumage.bmstu_enrollee;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigation;
+    private NavController navController;
+    private boolean backPressedOnce = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupFragments() {
-        NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment);
         bottomNavigation = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomNavigation, navController);
     }
@@ -33,5 +38,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void showBottomNav() {
         bottomNavigation.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavDestination destination = navController.getCurrentDestination();
+        if (destination != null &&
+                destination.getId() == navController.getGraph().getStartDestination()) {
+            if (backPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+            backPressedOnce = true;
+            Toast.makeText(this, getResources().getString(R.string.press_back_again),
+                    Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    backPressedOnce = false;
+                }
+            }, 2000);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
