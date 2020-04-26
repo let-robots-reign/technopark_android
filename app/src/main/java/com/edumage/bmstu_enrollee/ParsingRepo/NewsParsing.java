cb0bf8b0fdf5a0b7410485f01b650834d015dd53
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-26 18:02:02 +0400

    Parsing links fix, news_page layout fix

--- app/src/main/java/com/edumage/bmstu_enrollee/ParsingRepo/NewsParsing.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/ParsingRepo/NewsParsing.java
@@ -50,21 +50,36 @@ public class NewsParsing {
         Document doc = Jsoup.connect(URL).get();
         StringBuilder content = new StringBuilder();
 
-        Elements texts = doc.select("div.b-newsdetail-text").select("p,span,a");
+        Elements texts = doc.select("div.b-newsdetail-text").select("p,span");
         String result;
         for (Element text : texts) {
             result = text.text().replaceAll("\u00a0", "").trim();
             if (content.indexOf(result) == -1) {
                 content.append(result).append("\n\n");
             }
+            Element link_under_span = text.select("a").first();
+            if (link_under_span != null) {
+                String linkHref = link_under_span.attr("href")
+                        .replace("http:", "https:");
+                if (linkHref.charAt(0) == '/') {
+                    linkHref = BASE_URL + linkHref;
+                }
+
+                if (content.indexOf(linkHref) == -1) {
+                    content.append(linkHref).append("\n\n");
+                }
+            }
         }
         Elements links = doc.select("div.b-newsdetail-text > div.j-marg2").select("a");
         for (Element link : links) {
-            result = link.text().replaceAll("\u00a0", "").trim();
-            if (content.indexOf(result) == -1) {
-                content.append(result).append(":\n");
+            String resultText = link.text().replaceAll("\u00a0", "").trim();
+            String resultLink = link.attr("href")
+                    .replace("http:", "https:");
+
+            if (content.indexOf(resultText) == -1 && content.indexOf(resultLink) == -1) {
+                content.append(resultText).append(":\n");
+                content.append(BASE_URL).append(resultLink).append("\n");
             }
-            content.append(BASE_URL).append(link.attr("href")).append("\n");
         }
 
         return content.toString();
