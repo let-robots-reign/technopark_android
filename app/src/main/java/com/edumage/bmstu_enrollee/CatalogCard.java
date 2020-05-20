c597426ff98c8a7469df06ca7c5dabcae1370266
Alexey <zotov.alexei.1@yandex.ru>
2020-05-20 22:24:25 +0400

    'About' tab

--- app/src/main/java/com/edumage/bmstu_enrollee/CatalogCard.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/CatalogCard.java
@@ -1,9 +1,6 @@
 package com.edumage.bmstu_enrollee;
 
 public class CatalogCard {
-
-
-
     public static int ABOUT_CARD_ID = 0;
     public static int NEWS_CARD_ID = 1;
     public static int FACULTIES_CARD_ID = 2;
@@ -11,7 +8,6 @@ public class CatalogCard {
     public static int EVENTS_CARD_ID = 4;
     public static int APPLY_CARD_ID = 5;
     public static int INFO_CARD_ID = 6;
-
 
     private String title;
     private int image;
--- app/src/main/java/com/edumage/bmstu_enrollee/Fragments/CatalogFragment.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/Fragments/CatalogFragment.java
@@ -62,8 +62,8 @@ public class CatalogFragment extends Fragment implements CatalogCardsAdapter.OnC
 
     @Override
     public void onCardClick(int catalogId) {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
         if (catalogId == CatalogCard.ABOUT_CARD_ID) {
-            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.action_catalog_tab_to_universityFragment);
         } else if (catalogId == CatalogCard.NEWS_CARD_ID || catalogId == CatalogCard.EVENTS_CARD_ID) {
             Bundle args = new Bundle();
@@ -71,14 +71,13 @@ public class CatalogFragment extends Fragment implements CatalogCardsAdapter.OnC
                 args.putSerializable("type", FeedType.NEWS);
             else
                 args.putSerializable("type", FeedType.EVENTS);
-            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.action_catalogFragment_to_newsFragment, args);
         } else if (catalogId == CatalogCard.CAMPUS_CARD_ID) {
-            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.action_catalog_tab_to_buildingActivity);
         } else if (catalogId == CatalogCard.APPLY_CARD_ID) {
-            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.action_catalog_Fragment_to_applyFragment);
+        } else if (catalogId == CatalogCard.INFO_CARD_ID) {
+            navController.navigate(R.id.action_catalog_tab_to_informationFragment);
         }
     }
 }
