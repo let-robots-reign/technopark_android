e9a5a840c14c83be6b2ddfb246c798071017b24f
Alexey <zotov.alexei.1@yandex.ru>
2020-05-23 17:31:16 +0400

    add PDF Viewer

--- app/src/main/java/com/edumage/bmstu_enrollee/Fragments/CafedraPage.java
+++ app/src/main/java/com/edumage/bmstu_enrollee/Fragments/CafedraPage.java
@@ -8,30 +8,36 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import com.edumage.bmstu_enrollee.CafedraPageItem;
 import com.edumage.bmstu_enrollee.R;
 import com.edumage.bmstu_enrollee.XmlCafedraParser;
+import com.github.barteksc.pdfviewer.PDFView;
 
 import org.xmlpull.v1.XmlPullParserException;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class CafedraPage extends Fragment {
     private CafedraPageItem item;
     private String nameCaf;
     private static Map<String, String> disciplinesMap = createMap();
+    private PDFView pdfView;
+    private OnBackPressedCallback callback;
+    private OnBackPressedDispatcher backPressedDispatcher;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Bundle args = getArguments();
+        final Bundle args = getArguments();
         if (args != null) {
             String nameFacultet = args.getString("nameFacultet");
             int cafedraNumber = args.getInt("cafedraNumber");
@@ -43,6 +49,17 @@ public class CafedraPage extends Fragment {
                 e.printStackTrace();
             }
         }
+
+        callback = new OnBackPressedCallback(false) {
+            @Override
+            public void handleOnBackPressed() {
+                pdfView.setVisibility(View.GONE);
+                callback.setEnabled(false);
+            }
+        };
+
+        backPressedDispatcher = requireActivity().getOnBackPressedDispatcher();
+        backPressedDispatcher.addCallback(CafedraPage.this, callback);
     }
 
     @Nullable
@@ -50,28 +67,7 @@ public class CafedraPage extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.cafedra_page, container, false);
 
-        String name = item.getName();
-        String kod = item.getCode();
-        String describe = item.getDescription();
-        String plan = item.getPlan();
-        String link = item.getSiteLink();
-        String host = item.getHostel();
-
-        TextView fullNameCafedra = rootView.findViewById(R.id.full_name_cafedra);
-        TextView kodSpec = rootView.findViewById(R.id.kod_spec);
-        TextView fullDescribe = rootView.findViewById(R.id.full_describe);
-        TextView studyPlan = rootView.findViewById(R.id.plan_of_study);
-        TextView site = rootView.findViewById(R.id.link);
-        TextView hostel = rootView.findViewById(R.id.hostel);
-
-        fullNameCafedra.setText(name);
-        kodSpec.setText(kod);
-        fullDescribe.setText(describe);
-        studyPlan.setText(plan);
-        site.setText(link);
-        hostel.setText(host);
-
-        Toolbar toolbar = rootView.findViewById(R.id.toolbar);
+        final Toolbar toolbar = rootView.findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_left_arrow);
         toolbar.setTitle(nameCaf);
         toolbar.setTitleTextColor(Color.BLACK);
@@ -82,9 +78,52 @@ public class CafedraPage extends Fragment {
             }
         });
 
+        pdfView = rootView.findViewById(R.id.pdfView);
+        if (savedInstanceState == null) {
+            pdfView.setVisibility(View.GONE);
+        } else {
+            openPdf();
+        }
+
+        TextView fullNameCafedra = rootView.findViewById(R.id.full_name_cafedra);
+        TextView kodSpec = rootView.findViewById(R.id.kod_spec);
+        TextView fullDescribe = rootView.findViewById(R.id.full_describe);
+        TextView studyPlan = rootView.findViewById(R.id.plan_of_study);
+        TextView site = rootView.findViewById(R.id.link);
+        TextView hostel = rootView.findViewById(R.id.hostel);
+
+        fullNameCafedra.setText(item.getName());
+        kodSpec.setText(item.getCode());
+        fullDescribe.setText(item.getDescription());
+        studyPlan.setText(item.getPlan());
+        site.setText(item.getSiteLink());
+        hostel.setText(item.getHostel());
+
+        studyPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPdf();
+                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        backPressedDispatcher.onBackPressed();
+                    }
+                });
+                callback.setEnabled(true);
+            }
+        });
+
         return rootView;
     }
 
+
+    private void openPdf() {
+        pdfView.setVisibility(View.VISIBLE);
+        pdfView.fromAsset(item.getPlan())
+                .password(null)
+                .defaultPage(0)
+                .load();
+    }
 
     private static Map<String, String> createMap() {
         Map<String, String> map = new HashMap<>();
